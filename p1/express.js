53fd13ecc2d9d887c26b14730f374d30ab1b81e2
alam7865 <sabbajalam5@gmail.com>
2025-03-25 23:53:45 +0530

    Prac server

--- p1/express.js
+++ p1/express.js
@@ -4,6 +4,16 @@ app.get("/user", (req, res) => {
   console.log("HHHHHHHHH");
   res.send("Hi Sabaz122345");
 });
+
+app.get("/hero", (req, res) => {
+  console.log("Hero");
+  res.send("Hi Hero");
+});
+
+app.get("/apple", (req, res) => {
+  console.log("Apple");
+  res.send("Hi Apple");
+});
 app.listen(7777, () => {
   console.log("Server started at port 7777");
 });
